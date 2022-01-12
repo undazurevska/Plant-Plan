56705af48b44cee2fb2a791c2dc649a6ae6e274f
initalegzdina <95502251+initalegzdina@users.noreply.github.com>
2022-01-12 22:04:28 +0200

    Update colors.js

--- consts/colors.js
+++ consts/colors.js
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+onst COLORS = {
+  white: '#fff',
+  dark: '#000',
+  red: '#F52A2A',
+  light: '#F1F1F1',
+  green: '#5b7760',
+};
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default COLORS;
