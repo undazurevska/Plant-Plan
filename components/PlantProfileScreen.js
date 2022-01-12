7b6d53bc9323473a38e3ff981524c0172df33818
undazurevska <95476474+undazurevska@users.noreply.github.com>
2022-01-12 22:12:06 +0200

    Update PlantProfileScreen.js

--- components/PlantProfileScreen.js
+++ components/PlantProfileScreen.js
@@ -1,20 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {View, SafeAreaView, Image, Text, StyleSheet, TextInput, TouchableOpacity,Button} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import COLORS from '../consts/colors';
+import { useNavigate } from "react-router-dom";
 
-export default function App() {
+
+
+const PlantProfileScreen = ({navigation, route}) => {
+  const plant = route.params;
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: COLORS.white,
+      }}>
+      <View style={style.header}>
+        <Icon name="arrow-back" size={28} onPress={() => navigation.goBack()} />
+      </View>
+      <View style={style.imageContainer}>
+        <Image source={plant.img} style={{resizeMode: 'contain', flex: 1}} />
+      </View>
+      <View style={style.detailsContainer}>
+        <View
+          style={{
+            marginLeft: 20,
+            marginTop: 20,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}>
+          <Text style={{fontSize: 22, fontWeight: 'bold'}}>{plant.name}</Text>
+            <View>
+             <Button
+                  onPress={() => {navigation.navigate ('Calendar', {})} }
+                  title="Calendar" 
+                  color="#5b7760"
+              />
+            </View>
+        </View>
+        <View style={{paddingHorizontal: 20, marginTop: 10}}>
+          <Text style={{fontSize: 20, fontWeight: 'bold'}}>About</Text>
+          <TextInput placeholder="Write about your plant" style={style.input} />
+          <Text
+            style={{
+              color: 'grey',
+              fontSize: 16,
+              lineHeight: 22,
+              marginTop: 10,
+            }}>
+            {plant.about}
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+const style = StyleSheet.create({
+  header: {
+    paddingHorizontal: 20,
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  imageContainer: {
+    flex: 0.45,
+    marginTop: 20,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  detailsContainer: {
+    flex: 0.55,
+    backgroundColor: COLORS.light,
+    marginHorizontal: 7,
+    marginBottom: 7,
+    borderRadius: 20,
+    marginTop: 30,
+    paddingTop: 30,
+  },
+  input: {
+    color: 'grey',
+    fontSize: 16,
+    lineHeight: 22,
+    marginTop: 10,
   },
 });
+
+export default PlantProfileScreen;
+
