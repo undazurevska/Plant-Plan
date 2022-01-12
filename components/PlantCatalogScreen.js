caa3a99be1206981b23a3431178c08aead73cd56
undazurevska <95476474+undazurevska@users.noreply.github.com>
2022-01-12 22:11:30 +0200

    Update PlantCatalogScreen.js

--- components/PlantCatalogScreen.js
+++ components/PlantCatalogScreen.js
@@ -1,20 +1,121 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {
+  View,
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  FlatList,
+  Image,
+  Dimensions,
+} from 'react-native';
+import {TextInput, TouchableOpacity} from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import COLORS from '../consts/colors';
+import plants from '../consts/plants';
+const width = Dimensions.get('window').width / 2 - 30;
 
-export default function App() {
+const PlantCatalogScreen = ({navigation}) => {
+  const [catergoryIndex, setCategoryIndex] = React.useState(0);
+
+  const Card = ({plant}) => {
+    return (
+      <TouchableOpacity
+        activeOpacity={0.8}
+        onPress={() => navigation.navigate('Details', plant)}>
+        <View style={style.card}>
+          <View
+            style={{
+              height: 100,
+              alignItems: 'center',
+            }}>
+            <Image
+              source={plant.img}
+              style={{flex: 1, resizeMode: 'contain'}}
+            />
+          </View>
+
+          <Text style={{fontWeight: 'bold', fontSize: 17, marginTop: 10}}>
+            {plant.name}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <SafeAreaView
+      style={{flex: 1, paddingHorizontal: 20, backgroundColor: COLORS.white}}>
+      <View style={style.header}>
+          <Text style={{fontSize: 38, color: COLORS.green, fontWeight: 'bold'}}>
+            My Plants
+          </Text>
+      </View>
+      <View style={{marginTop: 30, flexDirection: 'row'}}>
+        <View style={style.searchContainer}>
+          <Icon name="search" size={25} style={{marginLeft: 20}} />
+          <TextInput placeholder="Search" style={style.input} />
 
-const styles = StyleSheet.create({
-  container: {
+        </View>
+        <View style={style.sortBtn}>
+          <Icon name="sort" size={30} color={COLORS.white} />
+        </View>
+      </View>
+      <FlatList
+        columnWrapperStyle={{justifyContent: 'space-between'}}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          marginTop: 10,
+          paddingBottom: 50,
+        }}
+        numColumns={2}
+        data={plants}
+        renderItem={({item}) => {
+          return <Card plant={item} />;
+        }}
+      />
+    </SafeAreaView>
+  );
+};
+
+const style = StyleSheet.create({
+  card: {
+    height: 190,
+    backgroundColor: COLORS.light,
+    width,
+    marginHorizontal: 10,
+    borderRadius: 10,
+    marginBottom: 20,
+    padding: 15,
+  },
+  header: {
+    marginTop: 30,
+    marginLeft: 30,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  searchContainer: {
+    height: 50,
+    marginHorizontal: 10,
+    backgroundColor: COLORS.light,
+    borderRadius: 10,
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'row',
     alignItems: 'center',
+  },
+  input: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    flex: 1,
+    color: COLORS.dark,
+  },
+  sortBtn: {
+    marginHorizontal: 10,
+    marginLeft: 10,
+    height: 50,
+    width: 50,
+    borderRadius: 10,
+    backgroundColor: COLORS.green,
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
+export default PlantCatalogScreen;
