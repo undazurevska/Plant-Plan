5195d3563786481d209020cd2f04a24b6c397a19
svitkadiii <95414945+svitkadiii@users.noreply.github.com>
2022-01-12 22:43:21 +0200

    Update CalendarScreen.js

--- components/CalendarScreen.js
+++ components/CalendarScreen.js
@@ -17,41 +17,43 @@ export default function CalendarScreen({navigation, route}) {
       <View style={style.header}>
         <Icon name="arrow-back" size={28} onPress={() => navigation.goBack()} />
       </View>
-      </SafeAreaView>,
-<CalendarList
-theme={{
-    backgroundColor: '#ffffff',
-    calendarBackground: '#F1F1F1',
-    textSectionTitleColor: '#000',
-    todayTextColor: 'green',
-    monthTextColor: '#000',
-  }}
-  onVisibleMonthsChange={(months) => {console.log('now these months are visible', months);}}
-  pastScrollRange={0}
-  futureScrollRange={5}
-  scrollEnabled={true}
-  showScrollIndicator={true}
-  current={new Date()}
-  minDate={'2022-01-01'}
-  maxDate={'2025-05-30'}
-   onDayPress={day => {
-    console.log('day pressed');
-    }}
-       markedDates={{
-    '2022-01-16': {selected: true, marked: true, selectedColor: '#5b7760'},
-    '2022-01-23': {selected: true, marked: true, selectedColor: '#5b7760'},
-    '2022-02-16': {selected: true, marked: true, selectedColor: '#5b7760'},
-    '2022-02-23': {selected: true, marked: true, selectedColor: '#5b7760'},
-    '2022-02-09': {selected: true, marked: true, selectedColor: '#5b7760'}
-  }}
-/>
-
+      <View>
+        <CalendarList
+          theme={{
+            backgroundColor: '#ffffff',
+            calendarBackground: '#F1F1F1',
+            textSectionTitleColor: '#000',
+            todayTextColor: 'green',
+            monthTextColor: '#000',
+          }}
+          onVisibleMonthsChange={(months) => {console.log('now these months are visible', months);}}
+            pastScrollRange={0}
+            futureScrollRange={5}
+            scrollEnabled={true}
+            showScrollIndicator={true}
+            current={new Date()}
+            minDate={'2022-01-01'}
+            maxDate={'2025-05-30'}
+            onDayPress={day => {
+            console.log('day pressed');
+          }}
+          markedDates={{
+            '2022-01-16': {selected: true, marked: true, selectedColor: '#5b7760'},
+            '2022-01-23': {selected: true, marked: true, selectedColor: '#5b7760'},
+            '2022-02-16': {selected: true, marked: true, selectedColor: '#5b7760'},
+            '2022-02-23': {selected: true, marked: true, selectedColor: '#5b7760'},
+            '2022-02-09': {selected: true, marked: true, selectedColor: '#5b7760'}
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 const style = StyleSheet.create({
   header: {
     paddingHorizontal: 20,
     marginTop: 20,
+    marginBottom: 20,
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
