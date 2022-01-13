833617e1cf4ada9cf51a0c160641629bba962e07
initalegzdina <95502251+initalegzdina@users.noreply.github.com>
2022-01-13 09:11:52 +0200

    Update App.js

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {StatusBar} from 'react-native';
+import COLORS from './consts/colors';
 
-const Stack = createStackNavigator();
 import HomeScreen from './components/PlantCatalogScreen';
 import DetailsSceeen from './components/PlantProfileScreen';
 import CalendarScreen from './components/CalendarScreen';
-import {StatusBar} from 'react-native';
-import COLORS from './consts/colors';
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
@@ -22,5 +23,4 @@ const App = () => {
     </NavigationContainer>
   );
 };
-
 export default App;
